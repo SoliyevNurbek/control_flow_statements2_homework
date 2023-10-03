af6ef44bbbfe713d5ccea2adc67c9a8ce0a77cb4
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-10-03 09:59:25 +0500

    e5

--- if05.py
+++ if05.py
@@ -6,4 +6,16 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    m=n%10
+    if n//10%10>m:
+        m=n//10%10
+    elif n//100%10>m:
+        m=n//100%10
+    elif n//1000%10>m:
+        m=n//1000%10
+    elif n//10000%10>m:
+        m=n//10000%10
+    elif n//100000%10>m:
+        m=n//100000%10
+    return m
+print(main(84956))
