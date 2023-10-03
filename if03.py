c3d20651250da4f5520803d80cf23618f32390a3
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-10-03 09:58:38 +0500

    e3

--- if03.py
+++ if03.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a-b>0 or b-a>0) and (a<c<b or a>c>b):
+        return c
+    if (c-b>0 or b-c>0) and (c<a<b or b>a>c):
+        return a
+    else :
+        return b
+print(main(140,80,25))
